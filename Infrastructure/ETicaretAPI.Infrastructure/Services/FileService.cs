de99962e70559800c70bdb969caeef39fecab60b
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-10-22 23:53:39 +0300

    operation

--- Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Services;
+using ETicaretAPI.Infrastructure.Operation;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
@@ -22,7 +23,7 @@ namespace ETicaretAPI.Infrastructure.Services
         {
             try
             {
-                await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+               await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
                 await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
@@ -35,9 +36,52 @@ namespace ETicaretAPI.Infrastructure.Services
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        private async Task<string> FileRenameAsync(string path, string fileName, bool first=true)
         {
-            throw new NotImplementedException();
+            string newFileName = await  Task.Run<string>(async () => 
+            {
+
+                string extension = Path.GetExtension(fileName);
+                string newFileName = string.Empty;
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+                }
+                else
+                {
+                    newFileName=fileName;
+                    int indexNo1 = newFileName.IndexOf('-');
+                    if(indexNo1 == -1)
+                    {
+                        newFileName =$"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
+                    }
+                    else
+                    {
+                        newFileName.IndexOf("-", indexNo1);
+
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo=newFileName.Substring(indexNo1, indexNo2-indexNo1-1);
+                        int _fileNo = int.Parse(fileNo);
+                        _fileNo++;
+                        newFileName = newFileName.Remove(indexNo1, indexNo2 - indexNo1 - 1)
+                                                 .Insert(indexNo1,_fileNo.ToString());
+                    }
+                }
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                {
+                    return await FileRenameAsync(path, newFileName,false);
+                }
+                else
+                {
+                    return newFileName;
+                }
+            });
+
+
+
+            return "";
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
@@ -55,7 +99,7 @@ namespace ETicaretAPI.Infrastructure.Services
             List<bool> results = new();
             foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath,file.FileName);
 
                 bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
